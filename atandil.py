6eacb4d6cd9b064f5593e1c96a661821c4cb69ab
Glynyon <glynyon+dragonsmoke@gmail.com>
2023-06-10 00:35:16 -0600

    formatting

--- atandil.py
+++ atandil.py
@@ -5,13 +5,13 @@ import os
 import json
 
 # handle tokens
-with open('./data/config.json') as config_file:
+with open("./data/config.json") as config_file:
     config = json.load(config_file)
 
 intents = nextcord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix = '$', intents=intents)
+client = commands.Bot(command_prefix="$", intents=intents)
 
 
 @client.event
@@ -19,16 +19,17 @@ async def on_ready():
     print("The bot is now ready for use!")
     print("-----------------------------")
 
-token = config['bot_token']
+
+token = config["bot_token"]
 test_server_id = 820053634598567984
 
 initial_extensions = []
 
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
         initial_extensions.append("cogs." + filename[:-3])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for extension in initial_extensions:
         client.load_extension(extension)
 
--- cogs/experience.py
+++ cogs/experience.py
@@ -2,26 +2,43 @@ import nextcord
 from nextcord.ext import commands
 from nextcord import Interaction, Member
 
+
 class Experience(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     test_server_id = 820053634598567984
 
-    @nextcord.slash_command(name="hello", description="My first application Command", guild_ids=[test_server_id])
+    @nextcord.slash_command(
+        name="hello",
+        description="My first application Command",
+        guild_ids=[test_server_id],
+    )
     async def test(self, interaction: Interaction):
         await interaction.response.send_message("Hello!")
 
-    @nextcord.slash_command(name="addxp", description="Adds experience to your character.", guild_ids=[test_server_id])
-    async def add_experience(self, interaction: Interaction, member: Member, experience: int):
+    @nextcord.slash_command(
+        name="addxp",
+        description="Adds experience to your character.",
+        guild_ids=[test_server_id],
+    )
+    async def add_experience(
+        self, interaction: Interaction, member: Member, experience: int
+    ):
         await interaction.response.send_message(f"Adds {experience}xp to {member}!")
-        
 
-    @nextcord.slash_command(name="removexp", description="Removes experience from your character.", guild_ids=[test_server_id])
-    async def remove_experience(self, interaction: Interaction, member: Member, experience: int):
-        await interaction.response.send_message(f"Removes {experience}xp from {member}!")
+    @nextcord.slash_command(
+        name="removexp",
+        description="Removes experience from your character.",
+        guild_ids=[test_server_id],
+    )
+    async def remove_experience(
+        self, interaction: Interaction, member: Member, experience: int
+    ):
+        await interaction.response.send_message(
+            f"Removes {experience}xp from {member}!"
+        )
 
 
 def setup(client):
     client.add_cog(Experience(client))
-
